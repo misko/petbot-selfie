a65ea75648c4402823fd776fa885e7abebb8bb61
misko <misko@cs.toronto.edu>
2014-08-23 15:57:42 +0000

    update to sound detect

--- send_atos.sh
+++ send_atos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+/bin/sh "/home/pi/petbot/play_sound.sh https://petbot.ca/static/sounds/jsw.mp3"
 sudo /home/pi/petbot/single_cookie/single_cookie 10
 /usr/bin/composite -gravity southeast -quality 100 tiny_logo.png $1 /dev/shm/petbot_selfie.jpg
 /usr/bin/mutt -s "Atos - This is me? $2"  -a /dev/shm/petbot_selfie.jpg -- mouse9911@gmail.com < /dev/null
