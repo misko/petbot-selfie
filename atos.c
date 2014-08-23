303e1512350b2c08a63aee0dea4c4ec1fd74bd36
misko <misko@cs.toronto.edu>
2014-08-23 00:03:17 +0000

    motion updats

--- atos.c
+++ atos.c
@@ -16,6 +16,11 @@
 #include <pthread.h>
 #include <string.h>
 
+#define WAIT_TIME 2
+#define LONG_WAIT_TIME 1500
+
+#define RMSE_THRESHOLD	3000
+
 float* predictions;
 int predictionsLength;
 char** predictionsLabels;
@@ -26,103 +31,226 @@ const char * predictorFileName;
 const char * imageFileName;
 
 
+void * networkHandle;
+void * predictor;
+
 int release=0;
+int exit_now=0;
 sem_t stopped;
 sem_t running;
 
+float rmse_pictures(char * fn1,char * fn2) {
+	//fswebcam here
+	int pipefd[2];
+	if (pipe(pipefd)==-1) {
+		fprintf(stderr,"ERROR IN PIPE CREATE\n");
+		exit(1);
+	}
+
+
+	int pid=fork();
+	if (pid==0) {
+		close(pipefd[0]);
+		//child
+		dup2(pipefd[1],2);
+		char * args[] = { "/usr/bin/compare", "-fuzz",  "5", "-metric",  "RMSE" , fn1,  fn2,  "/dev/null", NULL};
+		int r = execv(args[0],args);
+		fprintf(stderr,"SHOULD NEVER REACH HERE %d\n",r);
+	}
+	close(pipefd[1]);
+	//get the RMSE
+	char buffer[1024];
+	int r = read(pipefd[0], buffer, 1024);
+	if (r<=0) {
+		fprintf(stderr,"ERROR IN READING\n");
+		exit(1);
+	}
+	float rmse;
+	sscanf(buffer, "%f",&rmse);
+	//master
+	wait(NULL);	
+	return rmse;
+}
+int take_picture(char * fn) {
+	//fswebcam here
+	unlink(fn); //remove the file if it exists
+	while ( access( fn, F_OK ) == -1 ) {
+		/*if (release==1) {
+			break;
+		}*/
+		fprintf(stderr,"WARNING ABOVE!\n");	
+		fprintf(stderr, "taking picture\n");
+		int pid=fork();
+		if (pid==0) {
+			//child
+			//TODO make sure acquired image file
+			char * args[] = { "/usr/bin/fswebcam","-r","640x480","--skip","5",
+				" --no-underlay","--no-info","--no-banner","--no-timestamp","--quiet",fn, NULL };
+			int r = execv(args[0],args);
+			fprintf(stderr,"SHOULD NEVER REACH HERE %d\n",r);
+		}
+		//master
+		wait(NULL);	
+	}
+	return 0;
+}
+
+
+void busy_wait(int s) {
+	while (s>0) {
+		sleep(1);
+		if (release==1) {
+			return;
+		}
+		s--;
+	}
+}
+
+
+
+int check_for_dog(char * fn ) {
+	if (release==1) {
+		return 0;
+	}	
+	void * imageHandle = jpcnn_create_image_buffer_from_file(fn);
+	if (imageHandle == NULL) {
+		fprintf(stderr, "DeepBeliefSDK: Couldn't load image file '%s'\n", fn);
+		return 0;
+	}
+
+	//next classify
+	if (release==1) {
+		return 0;
+	}	
+
+	jpcnn_classify_image(networkHandle, imageHandle, 0, layer, &predictions, &predictionsLength, &predictionsLabels, &predictionsLabelsLength);
+	jpcnn_destroy_image_buffer(imageHandle);
+
+
+	//next predict
+	if (release==1) {
+		return 0;
+	}	
+	
+	float pred = jpcnn_predict(predictor, predictions, predictionsLength);
+	fprintf(stdout,"Atos probability %f\n",pred);
+
+	//next predict
+	if (release==1) {
+		return 0;
+	}	
+
+	//next send out the image if it passes
+	if (pred>0.15) {
+		char pred_s[1024];
+		sprintf(pred_s,"%0.4f", pred);
+		int pid=fork();
+		if (pid==0) {
+			//child
+			char * args[] = { "/bin/bash","./send_atos.sh",imageFileName, pred_s, NULL };
+			int r = execv(args[0],args);
+			fprintf(stderr,"SHOULD NEVER REACH HERE %d\n",r);
+		}
+		//master
+		wait(NULL);	
+		return 1;
+	}
+	return 0;
+}
+
+
+
+
+
 void * analyze() {
+	char currentImageFileName[1024];
+	char previousImageFileName[1024];
+	sprintf(currentImageFileName,"%s_current.jpg",imageFileName);
+	sprintf(previousImageFileName,"%s_previous.jpg",imageFileName);
 
 
-   fprintf(stderr,"Loading network %s\n",networkFileName);
-  void * networkHandle = jpcnn_create_network(networkFileName);
-  if (networkHandle == NULL) {
-    fprintf(stderr, "DeepBeliefSDK: Couldn't load network file '%s'\n", networkFileName);
-    return;
-  }
-   fprintf(stderr,"Loading predictor %s\n",predictorFileName);
-	  void * predictor = jpcnn_load_predictor(predictorFileName);
-	  if (predictor==NULL) {
+	fprintf(stderr,"Loading network %s\n",networkFileName);
+	networkHandle = jpcnn_create_network(networkFileName);
+	if (networkHandle == NULL) {
+		fprintf(stderr, "DeepBeliefSDK: Couldn't load network file '%s'\n", networkFileName);
+		return;
+	}
+	fprintf(stderr,"Loading predictor %s\n",predictorFileName);
+	predictor = jpcnn_load_predictor(predictorFileName);
+	if (predictor==NULL) {
 		fprintf(stderr,"Failed to load predictor\n");
 		return;
-	  }
-  while (1>0) {
-	  sem_wait(&running);
+	}
 
-	  while (1>0) {
+	//main loop
+	while (1>0) {
+		if (exit_now==1) {
+			break;
+		}
+		sem_wait(&running);
+		
+		
+		//detect-and-work loop
+		while (1>0) {
+			
+			if (release==1) {
+				break;
+			}
 	
-		//fswebcam here
-		unlink(imageFileName); //remove the file if it exists
-		while ( access( imageFileName, F_OK ) == -1 ) {
+			//first picture
+			take_picture(currentImageFileName);
+
+			busy_wait(WAIT_TIME);
 			if (release==1) {
 				break;
 			}	
-			fprintf(stderr, "taking picture\n");
-			int pid=fork();
-			if (pid==0) {
-				//child
-				//TODO make sure acquired image file
-				char * args[] = { "/usr/bin/fswebcam","-r","640x480","--skip","10",
-					" --no-underlay","--no-info","--no-banner","--no-timestamp","--quiet",imageFileName, NULL };
-				int r = execv(args[0],args);
-				fprintf(stderr,"SHOULD NEVER REACH HERE %d\n",r);
+			
+			//motion loop
+			int diff=0;
+			int motion=0;
+			while (diff<2500) {
+				if (release==1) {
+					break;
+				}	
+				//move current to previous
+				rename(currentImageFileName,previousImageFileName);
+		
+				//get current picture
+				take_picture(currentImageFileName);
+				//if no motion the wait a bit
+				if (motion<=0) {
+					busy_wait(WAIT_TIME);
+				} else {
+					motion--;
+				}
+
+				if (release==1) {
+					break;
+				}
+			
+				//compare
+				float rmse = rmse_pictures(currentImageFileName,previousImageFileName);
+				fprintf(stderr,"RMSE is %f\n",rmse);
+
+				//check for motion
+				if (rmse>RMSE_THRESHOLD) {
+					fprintf(stderr,"passed threshold moving on to detector...\n");;
+					motion=10;
+					int check = check_for_dog(currentImageFileName);	
+					if (check==1) {
+						busy_wait(LONG_WAIT_TIME);
+					}
+				}
 			}
-			//master
-			wait(NULL);	
 		}
+	  	sem_post(&stopped);
 
+  	}
 
-		//next load image
-		if (release==1) {
-			break;
-		}	
-
-		  void * imageHandle = jpcnn_create_image_buffer_from_file(imageFileName);
-		  if (imageHandle == NULL) {
-		    fprintf(stderr, "DeepBeliefSDK: Couldn't load image file '%s'\n", imageFileName);
-		    continue;
-		  }
-		
-		//next classify
-		if (release==1) {
-			break;
-		}	
-
-
-		  jpcnn_classify_image(networkHandle, imageHandle, 0, layer, &predictions, &predictionsLength, &predictionsLabels, &predictionsLabelsLength);
-		
-		//next predict
-		if (release==1) {
-			break;
-		}	
-		  float pred = jpcnn_predict(predictor, predictions, predictionsLength);
-		  fprintf(stdout,"%f\n",pred);
-
-		//next send out the image if it passes
-		  if (pred>0.15 && release==0) {
-
-			char pred_s[1024];
-			sprintf(pred_s,"%0.4f", pred);
-			int pid=fork();
-			if (pid==0) {
-				//child
-				char * args[] = { "/bin/bash","./send_atos.sh",imageFileName, pred_s, NULL };
-				int r = execv(args[0],args);
-				fprintf(stderr,"SHOULD NEVER REACH HERE %d\n",r);
-			}
-			//master
-			wait(NULL);	
-		  }
-		  jpcnn_destroy_image_buffer(imageHandle);
-	  }
-
-
-
-
-	  sem_post(&stopped);
-
-  }
-	  jpcnn_destroy_predictor(predictor);
-	 jpcnn_destroy_network(networkHandle);
+	fprintf(stderr, "Cleaning up predictor...\n");
+	jpcnn_destroy_predictor(predictor);
+	fprintf(stderr,"Cleaning up network...\n");
+	jpcnn_destroy_network(networkHandle);
 
 }
 
@@ -167,7 +295,6 @@ int main(int argc, const char * argv[]) {
 		if (strlen(buffer)>0 ) {
 			buffer[strlen(buffer)-1]='\0';
 		}
-		fprintf(stderr, "GOT %s\n",buffer);
 		if (strcmp(buffer,"STOP")==0) {
 			if (release==1) {
 				//hmmmm	
@@ -178,7 +305,6 @@ int main(int argc, const char * argv[]) {
 				fprintf(stdout,"STOPPED\n");	
 			}
 		} else if (strcmp(buffer,"GO")==0) {
-			fprintf(stderr, "GOT GO %s\n",buffer);
 			if (release==0) {
 
 			} else {
@@ -186,6 +312,7 @@ int main(int argc, const char * argv[]) {
 				sem_post(&running);
 			}	
 		} else if (strcmp(buffer,"EXIT")==0) {
+			exit_now=1;
 			if (release==1) {
 				//hmmmm	
 			} else {
--- send_atos.sh
+++ send_atos.sh
@@ -3,4 +3,3 @@
 sudo /home/pi/petbot/single_cookie/single_cookie 10
 /usr/bin/mutt -s "Atos - This is me? $2"  -a $1 -- mouse9911@gmail.com < /dev/null
 
-sleep 1800
