54a59dbe92bf2a3832860d3f45e0f5919aa2a6b3
misko <misko@cs.toronto.edu>
2014-08-26 05:55:24 +0000

    adding networks

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 gcc `pkg-config gstreamer-1.0 --cflags` -I/opt/vc/include/IL/ gst-record.c -o gst-record `pkg-config gstreamer-1.0 --libs` -O3 -g -Wall
-gcc -o atos atos.c  -L/usr/lib -ljpcnn
+gcc -o atos atos.c  -L/usr/lib -ljpcnn -lpthread
